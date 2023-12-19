23ebe2c83d552f7ebc065a80b9234c3c94b00d33
Tanishq Maheshwari <tanishqm2@gmail.com>
2023-12-19 19:29:54 +0100

    first commit, working QR code scanner and chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ChatInterface } from './ChatInterface';
+import CameraFeed from './CameraFeed';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CameraFeed />
+      {/*<ChatInterface />*/}
     </div>
   );
 }
